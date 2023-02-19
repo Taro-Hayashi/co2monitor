cebfaab21fc379b3f1b4c7be9645da7f1739dcd3
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2023-02-20 07:35:58 +0900

    code

---
+++ qmk_firmware/keyboards/co2monitor/README.md
@@ -0,0 +1,6 @@
+
+# CO2 Monitor
+- Manual: https://github.com/Taro-Hayashi/co2monitor/blob/main/README.md
+- BOOTH: https://tarohayashi.booth.pm/items/4500742
+  
+![](])  
---
+++ qmk_firmware/keyboards/co2monitor/config.h
@@ -0,0 +1,8 @@
+// Copyright 2021 Hayashi (@w_vwbw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//#include "config_common.h"
+
+#define OLED_FONT_H "keyboards/tarohayashi/co2monitor/glcdfont.c"
