56295734342b21a999f92c478d4be4c3eb2ec2a9
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2023-02-28 11:49:25 +0900

    delete code

--- qmk_firmware/keyboards/tarohayashi/co2monitor/config.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 Hayashi (@w_vwbw)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-//#include "config_common.h"
-
-#define OLED_FONT_H "keyboards/tarohayashi/co2monitor/glcdfont.c"
